fdf3eee8c85070b87b649847bb87e75e8f30cf5e
thebyronc <thebyronc@gmail.com>
2019-10-07 16:07:27 -0700

    add new logo designs

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 import { colors } from '../../style-utilities/variables';
 import Byron from '../../images/logo.png';
-
+import SVG from '../../images/resized.svg';
 const StyledNavigation = styled.nav`
     /* background-color: ${colors.purple}; */
     /* height: 75vh;
@@ -17,8 +17,8 @@ class Navigation extends Component {
             <StyledNavigation className="navbar navbar-light">
 
                 <a class="navbar-brand" href="#">
-                    <img src={Byron} height="32" class="d-inline-block align-top" alt="" />
-                    Byron Chang
+                    <img src={SVG} height="32" class="d-inline-block align-top" alt="" />
+                    
                 </a>
 
             </StyledNavigation>
