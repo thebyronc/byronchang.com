53c1f8bacdde5d8f2676993fba413bdd2476bf12
thebyronc <thebyronc@gmail.com>
2019-10-31 11:52:13 -0700

    add page formatting

--- src/style-utilities/variables.js
+++ src/style-utilities/variables.js
@@ -1,18 +1,17 @@
 export const colors = {
-    orange: '#F78E1E',
-    blue: '#00A1DF',
-    volt: '#CEFF00',
-    purple: '#757AD1',
-    grey: '#282828',
-    white: '#F1F1F1',
-    teal: '#4AD9D9',
-    red: '#F2385A'
+  orange: '#F78E1E',
+  blue: '#00A1DF',
+  volt: '#CEFF00',
+  purple: '#757AD1',
+  grey: '#282828',
+  white: '#F1F1F1',
+  teal: '#4AD9D9',
+  red: '#F2385A'
 };
 
 export const breakpoints = {
-    mobile: '576px',
-    mobilemax: '767',
-    tablet: '768px',
-    desktop: '992px',
-    ultraWideDesktop: '1200px'
-  };
+  mobile: '576px',
+  tablet: '768px',
+  desktop: '992px',
+  ultraWideDesktop: '1200px'
+};
